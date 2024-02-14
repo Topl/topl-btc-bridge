2fbb38fcfbee0cff7852fec1a76a81f159b5a6bb
Edmundo Lopez <e.lopez@topl.me>
2024-02-14 16:08:51 -0400

    TSDK-739 Test run processes

--- integration/src/test/scala/co/topl/bridge/BridgeIntegrationSpec.scala
+++ integration/src/test/scala/co/topl/bridge/BridgeIntegrationSpec.scala
@@ -8,7 +8,7 @@ class BridgeIntegrationSpec extends CatsEffectSuite {
 
   val DOCKER_CMD = "docker"
 
-  val generateNewAddressCommand = Seq(
+  val createWallet = Seq(
     "exec",
     "bitcoin",
     "bitcoin-cli",
@@ -17,20 +17,49 @@ class BridgeIntegrationSpec extends CatsEffectSuite {
     "createwallet",
     "wallet_name=testwallet"
   )
+  val getNewaddress = Seq(
+    "exec",
+    "bitcoin",
+    "bitcoin-cli",
+    "-regtest",
+    "-rpcwallet=testwallet",
+    "getnewaddress"
+  )
+  def generateToAddress(address: String) = Seq(
+    "exec",
+    "bitcoin",
+    "bitcoin-cli",
+    "-regtest",
+    "-rpcwallet=testwallet",
+    "generatetoaddress",
+    "generatetoaddress",
+    "101",
+    address
+  )
 
   test("Bridge should mint assets on the Topl network") {
     assertIO(
       for {
+        _ <- process
+          .ProcessBuilder(DOCKER_CMD, createWallet: _*)
+          .spawn[IO]
+          .use { _.exitValue }
         newAddress <- process
-          .ProcessBuilder(DOCKER_CMD, generateNewAddressCommand: _*)
+          .ProcessBuilder(DOCKER_CMD, getNewaddress: _*)
           .spawn[IO]
           .use { process =>
-            process.stderr
+            process.stdout
               .through(fs2.text.utf8Decode)
               .compile
               .foldMonoid
           }
         _ <- IO(println("New address: " + newAddress))
+        _ <- process
+          .ProcessBuilder(DOCKER_CMD, generateToAddress(newAddress): _*)
+          .spawn[IO]
+          .use { process =>
+            process.exitValue
+          }
       } yield (),
       ()
     )
