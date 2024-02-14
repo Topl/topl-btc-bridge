08f0c1fdbe81c66e26253d193aefb78a4e41fb93
Edmundo Lopez <e.lopez@topl.me>
2024-02-14 15:41:16 -0400

    TSDK-739 Test run processes

--- integration/src/test/scala/co/topl/bridge/BridgeIntegrationSpec.scala
+++ integration/src/test/scala/co/topl/bridge/BridgeIntegrationSpec.scala
@@ -1,9 +1,39 @@
 package co.topl.bridge
 
 import munit.CatsEffectSuite
+import fs2.io.process
+import cats.effect.IO
 
 class BridgeIntegrationSpec extends CatsEffectSuite {
+
+  val DOCKER_CMD = "docker"
+
+  val generateNewAddressCommand = Seq(
+    "exec",
+    "bitcoin",
+    "bitcoin-cli",
+    "-regtest",
+    "-named",
+    "createwallet",
+    "wallet_name=testwallet"
+  )
+
   test("Bridge should mint assets on the Topl network") {
     assert(true)
+    assertIO(
+      for {
+        newAddress <- process
+          .ProcessBuilder(DOCKER_CMD, generateNewAddressCommand: _*)
+          .spawn[IO]
+          .use { process =>
+            process.stdout
+              .through(fs2.text.utf8Decode)
+              .compile
+              .foldMonoid
+          }
+        _ <- IO(println(newAddress))
+      } yield (),
+      ()
+    )
   }
 }
