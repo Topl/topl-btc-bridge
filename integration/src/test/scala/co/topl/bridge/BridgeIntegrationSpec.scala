743bc3734c9d2d6febb4bb24c038e550f0f372b3
Edmundo Lopez <e.lopez@topl.me>
2024-07-03 13:49:46 -0400

    TSDK-820 Uncomment tests

--- integration/src/test/scala/co/topl/bridge/BridgeIntegrationSpec.scala
+++ integration/src/test/scala/co/topl/bridge/BridgeIntegrationSpec.scala
@@ -261,42 +261,43 @@ class BridgeIntegrationSpec
 
   override def munitFixtures = List(startServer)
 
-  // cleanupDir.test("Bridge should correctly peg-in BTC") { _ =>
-  //   info"Bridge should correctly peg-in BTC" >> successfulPegin()
-  // }
-  // cleanupDir.test("Bridge should fail correctly when user does not send BTC") {
-  //   _ =>
-  //     info"Bridge should fail correctly when user does not send BTC" >> failedPeginNoDeposit()
-  // }
-  // cleanupDir.test("Bridge should fail correctly when tBTC not minted") { _ =>
-  //   info"Bridge should fail correctly when tBTC not minted" >> failedPeginNoMint()
-  // }
-  // cleanupDir.test("Bridge should fail correctly when tBTC not redeemed") { _ =>
-  //   info"Bridge should fail correctly when tBTC not redeemed" >> failedRedemption()
-  // }
-
-  // cleanupDir.test(
-  //   "Bridge should correctly go back from PeginSessionWaitingForEscrowBTCConfirmation"
-  // ) { _ =>
-  //   info"Bridge should correctly go back from PeginSessionWaitingForEscrowBTCConfirmation" >> failedPeginNoDepositWithReorg()
-  // }
-
-  // cleanupDir.test(
-  //   "Bridge should correctly go back from PeginSessionWaitingForClaimBTCConfirmation"
-  // ) { _ =>
-  //   info"Bridge should correctly go back from PeginSessionWaitingForClaimBTCConfirmation" >> successfulPeginWithClaimError()
-  // }
-
-  // cleanupDir.test(
-  //   "Bridge should correctly retry if claim does not succeed"
-  // ) { _ =>
-  //   info"Bridge should correctly retry if claim does not succeed" >> successfulPeginWithClaimErrorRetry()
-  // }
+  cleanupDir.test("Bridge should correctly peg-in BTC") { _ =>
+    info"Bridge should correctly peg-in BTC" >> successfulPegin()
+  }
+  cleanupDir.test("Bridge should fail correctly when user does not send BTC") {
+    _ =>
+      info"Bridge should fail correctly when user does not send BTC" >> failedPeginNoDeposit()
+  }
+  cleanupDir.test("Bridge should fail correctly when tBTC not minted") { _ =>
+    info"Bridge should fail correctly when tBTC not minted" >> failedPeginNoMint()
+  }
+  cleanupDir.test("Bridge should fail correctly when tBTC not redeemed") { _ =>
+    info"Bridge should fail correctly when tBTC not redeemed" >> failedRedemption()
+  }
 
   cleanupDir.test(
-    "Bridge should correctly go back to minting if there is a reorg"
+    "Bridge should correctly go back from PeginSessionWaitingForEscrowBTCConfirmation"
   ) { _ =>
-    info"Bridge should correctly go back to minting if there is a reorg" >> failedMintingReorgModule()
+    info"Bridge should correctly go back from PeginSessionWaitingForEscrowBTCConfirmation" >> failedPeginNoDepositWithReorg()
   }
+
+  cleanupDir.test(
+    "Bridge should correctly go back from PeginSessionWaitingForClaimBTCConfirmation"
+  ) { _ =>
+    info"Bridge should correctly go back from PeginSessionWaitingForClaimBTCConfirmation" >> successfulPeginWithClaimError()
+  }
+
+  cleanupDir.test(
+    "Bridge should correctly retry if claim does not succeed"
+  ) { _ =>
+    info"Bridge should correctly retry if claim does not succeed" >> successfulPeginWithClaimErrorRetry()
+  }
+
+  // FIXME: Re-enable this test
+  // cleanupDir.test(
+  //   "Bridge should correctly go back to minting if there is a reorg"
+  // ) { _ =>
+  //   info"Bridge should correctly go back to minting if there is a reorg" >> failedMintingReorgModule()
+  // }
 
 }
